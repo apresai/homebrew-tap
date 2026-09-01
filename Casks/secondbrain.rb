7db06bd52c9d495c764628a07673b21a6addd6f5
Chad Neal <chad.neal@gmail.com>
2026-09-01 23:36:11 +0200

    SecondBrain v0.21.0: notarized cask (sha 7ae20350ccaa)

--- Casks/secondbrain.rb
+++ Casks/secondbrain.rb
@@ -1,6 +1,6 @@
 cask "secondbrain" do
-  version "0.20.1"
-  sha256 "3bdd3969176312ce049476f6185020b4425c16c7c4d5ef4421a3ae985ccb7fd5"
+  version "0.21.0"
+  sha256 "7ae20350ccaa5172544d07b147d2201695a7bbf9cc48604369ee7971a0d3b7f5"
 
   url "https://github.com/apresai/2ndbrain/releases/download/v#{version}/SecondBrain-#{version}-arm64.dmg"
   name "SecondBrain"
